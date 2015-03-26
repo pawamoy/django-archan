1d4979358d76516531fbfd565a0efbe511c60383
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2015-03-26 22:38:18 +0100

    Wip adapting django-archan to dependenpy

--- darchan/models.py
+++ darchan/models.py
@@ -2,33 +2,22 @@
 import json
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from dependenpy.utils import DependencyMatrix
+# from dependenpy.utils import MatrixBuilder
 
 
-class DependencyMatrixModel(models.Model):
-    json = models.TextField(_('JSON serialized matrix'))
+class MatrixModel(models.Model):
+    json_data = models.TextField(_('JSON serialized matrix'))
+    depth = models.PositiveSmallIntegerField(_('Depth'))
+    size = models.PositiveIntegerField(_('Size'))
+    builder = models.ForeignKey('MatrixBuilderModel',
+                                verbose_name=_('Builder'),
+                                related_name='matrices')
+
+
+class MatrixBuilderModel(models.Model):
     created = models.DateTimeField(_('Creation date'), auto_now=True)
-
-    def to_obj(self):
-        return json.loads(self.json)
-
-    def get_instance(self):
-        data = self.to_obj()
-        dm = DependencyMatrix(data['packages'])
-        dm.groups = data['groups']
-        dm.modules = data['modules']
-        dm.imports = data['imports']
-        dm.matrices = data['matrices']
-        dm.max_depth = data['max_depth']
-        dm._inside = data['_inside']
-        dm._modules_are_built = data['_modules_are_built']
-        dm._imports_are_built = data['_imports_are_built']
-        dm._matrices_are_built = data['_matrices_are_built']
-        return dm
-
-    def level(self, level):
-        return json.dumps(
-            self.get_instance().matrix_to_json(level, FILTER_OPTIONS))
+    groups = models.TextField(_('Groups', ))
+    max_depth = models.PositiveSmallIntegerField(_('Maximum depth'))
 
     def __unicode__(self):
         return '%s' % self.created
--- darchan/utils.py
+++ darchan/utils.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 import sys
-from dependenpy.utils import DependencyMatrix
-from darchan.models import DependencyMatrixModel
+import json
+from dependenpy.utils import MatrixBuilder
+from darchan.models import MatrixModel, MatrixBuilderModel
 
 
 def get_django_module_path(mod):
+    """A method to determine the path of a module within a Django project.
+
+    :param mod: str, the module name
+    :return: str, the absolute path to the module
+    """
+
     mod_path = sys.modules.get(mod)
     if mod_path:
         return 'py'.join(mod_path.__file__.rsplit('pyc'))
@@ -12,6 +19,46 @@ def get_django_module_path(mod):
 
 
 def generate_matrix(apps):
-    matrix = DependencyMatrix(apps, get_django_module_path)
-    matrix.build()
-    return DependencyMatrixModel.objects.create(json=matrix.to_json())
+    """Build a matrix using MatrixBuilder from dependenpy,
+    compute its different sorting orders, and return a database
+    instance of the builder and its matrices.
+
+
+    :param apps: str/list/OrderedDict, the list of packages to scan
+    :return: :class:`MatrixBuilderModel`, [:class:`MatrixModel`]; the builder
+        and its matrices (database objects)
+    """
+
+    builder = MatrixBuilder(apps, get_django_module_path)
+    builder.build()
+    for matrix in builder.matrices:
+        matrix.compute_orders()
+    return create_instance(builder)
+
+
+def create_instance(builder):
+    """Save the builder instance and its matrices as database objects
+    and return them.
+
+    :type builder: :class:`MatrixBuilder`
+    :param builder: the builder instance from dependenpy
+    :return: :class:`MatrixBuilderModel`, [:class:`MatrixModel`]; the builder
+        and its matrices (database objects)
+    """
+
+    builder_db = MatrixBuilderModel.objects.create(
+        max_depth=builder.max_depth,
+        groups=json.dumps(builder.groups))
+
+    matrices_db = []
+    for matrix in builder.matrices:
+        matrices_db.append(
+            MatrixModel.objects.create(
+                depth=matrix.depth,
+                size=matrix.size,
+                builder=builder_db,
+                json_data=json.dumps(
+                    {'modules': matrix.modules,
+                     'dependencies': matrix.dependencies})))
+
+    return builder_db, matrices_db
