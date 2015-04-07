a08de5850e0fcf00b5eb5c675f8d908f685481ff
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2015-04-07 16:22:25 +0200

    Ready for version 0.0.4, with wheel

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-archan',
-    version='0.0.3',
+    version='0.0.4',
     packages=['darchan'],
     include_package_data=True,
     license='MPL 2.0',
